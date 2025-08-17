e272455e9e47ba79ae6e0c5f06955aaa60a8bd25
faizkhan731 <faizkhan78776@gmail.com>
2025-08-17 11:45:49 +0530

    fth

--- backend/server.js
+++ backend/server.js
@@ -54,12 +54,21 @@ app.use(
 );
 
 // ✅ MySQL Connection
+// const db = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "chicken",
+// });
+
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "chicken",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
+
 db.connect((err) => {
   if (err) {
     console.error("Error connecting DB:", err);
